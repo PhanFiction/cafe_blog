aa7bb0d8d1366633cfefc5a749dd543985e68cc4
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-08-17 23:43:16 -0700

    Updated blogDB

--- db/blogDB.js
+++ db/blogDB.js
@@ -1,27 +1,27 @@
-const { Pool } = require('./pool');
+const pool = require('./pool');
 
 exports.fetchBlogs = async () => {
-  const { rows } = await Pool.query("SELECT * FROM blogs");
+  const { rows } = await pool.query("SELECT * FROM blogs");
   return rows;
 };
 
 exports.fetchBlogById = async (id) => {
-  const { rows } = await Pool.query("SELECT * FROM blogs WHERE id = $1", [id]);
+  const { rows } = await pool.query("SELECT * FROM blogs WHERE id = $1", [id]);
   return rows[0];
 };
 
 exports.createBlog = async (blogData) => {
-  const { title, content, img, author_id } = blogData;
-  const { rows } = await Pool.query(
-    "INSERT INTO blogs (title, content, img, author_id) VALUES ($1, $2, $3, $4) RETURNING *",
-    [title, content, img, author_id]
+  const { title, content, img, user_id } = blogData;
+  const { rows } = await pool.query(
+    "INSERT INTO blogs (title, content, img, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
+    [title, content, img, user_id]
   );
   return rows[0];
 };
 
 exports.updateBlog = async (id, blogData) => {
   const { title, content, img } = blogData;
-  const { rows } = await Pool.query(
+  const { rows } = await pool.query(
     "UPDATE blogs SET title = $1, content = $2, img = $3 WHERE id = $4 RETURNING *",
     [title, content, img, id]
   );
@@ -29,7 +29,7 @@ exports.updateBlog = async (id, blogData) => {
 };
 
 exports.deleteBlog = async (id) => {
-  const { rows } = await Pool.query("DELETE FROM blogs WHERE id = $1 RETURNING *", [id]);
+  const { rows } = await pool.query("DELETE FROM blogs WHERE id = $1 RETURNING *", [id]);
   return rows[0];
 };
 
