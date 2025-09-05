d606bb0e812e268434c059069e05063bf961488b
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-09-04 23:39:17 -0700

    Added middleware to blog routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,10 @@
 const blogController = require('../controller/blogController');
+const middleware = require('../middleware/index');
 const router = require('express').Router();
 
-router.post('/create', blogController.createBlog);
-router.put('/update/:id', blogController.updateBlog);
-router.delete('/delete/:id', blogController.deleteBlog);
+router.post('/create', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.createBlog);
+router.put('/update/:id', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.updateBlog);
+router.delete('/delete/:id', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.deleteBlog);
 router.get('/:id', blogController.getBlogById);
 router.get('/', blogController.getAllBlogs);
 
