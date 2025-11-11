247f9646827e45bb7f85f59bc623ed6660636811
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-11-10 21:56:11 -0800

    Added function to check if user owns the blog

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 // Extract token from the request and verify it
 const verifyToken = require('../utils/verifyToken');
+const blogDB = require('../db/blogDB');
 
 exports.extractToken = (req, res, next) => {
   const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
@@ -10,5 +11,18 @@ exports.extractToken = (req, res, next) => {
   req.authorized = true;
   req.userId = decodedToken.id;
 
+  next();
+};
+
+// Checks to make sure the blog creator matches the user id of who created the blog
+exports.checkBlogOwnerShip = async (req, res, next) => {
+  const blogId = req.params.id;
+  const userId = req.userId;
+
+  const foundBlog = await blogDB.fetchBlogById(blogId);
+
+  if (foundBlog.user_id !== userId) return res.status(403).json({ error: 'Forbidden' });
+
+  console.log(foundBlog);
   next();
 };
