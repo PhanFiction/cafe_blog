55c5314e7bda522ad3c8e73f287a2fb406b177cb
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-12-11 23:32:21 -0800

    Added two new functions to middleware file

--- middleware/index.js
+++ middleware/index.js
@@ -26,3 +26,26 @@ exports.checkBlogOwnerShip = async (req, res, next) => {
   console.log(foundBlog);
   next();
 };
+
+// Checks to make sure recipe creator matches the user id of who created the recipe
+exports.checkRecipeOwnership = async (req, res, next) => {
+  const recipeId = req.params.id;
+  const userId = req.userId;
+
+  const foundRecipe = await recipeDB.fetchRecipeById(recipeId);
+
+  if (foundRecipe.user_id !== userId) return res.status(403).json({ error: 'Forbidden' });
+
+  console.log(foundRecipe);
+  next();
+};
+
+exports.validateRecipe = (req, res, next) => {
+  const { title, ingredients, instructions, steps, description } = req.body;
+
+  if (!title || !ingredients || !instructions || !steps || !description) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  next();
+};
