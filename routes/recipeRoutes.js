811515b963afc2284396b4117f56b91c78a00107
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-12-12 23:43:08 -0800

    Added middleware

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,10 +1,10 @@
-const recipeController = require('../controllers/recipeController');
+const recipeController = require('../controller/recipeController');
 const middleware = require('../middleware/index');
 const router = require('express').Router();
 
 // Routes for recipe operations
 router.get('/', recipeController.getAllRecipes);
-router.post('/', middleware.validateRecipe, recipeController.createRecipe);
+router.post('/create', middleware.extractToken, recipeController.createRecipe);
 router.put('/:id', middleware.validateRecipe, middleware.checkRecipeOwnership, recipeController.updateRecipe);
 router.delete('/:id', middleware.checkRecipeOwnership, recipeController.deleteRecipe);
 
