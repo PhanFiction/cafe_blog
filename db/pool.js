be4d335adabf343e41e0bba66f9b36c59a909fa4
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-12-26 23:29:39 -0800

    Updated pool file for psql

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require('pg');
 const config = require('../config/index');
 
 module.exports = new Pool({
-  connectionString: `postgresql://${config.DB_USER}:${config.DB_PASSWORD}@localhost:${config.DB_PORT}/${config.DB_NAME}`
+  connectionString: config.databaseURL,
 });
