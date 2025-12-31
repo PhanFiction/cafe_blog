7b0631c2153a3ed4e601fad249a549af38e908b5
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-12-30 23:08:55 -0800

    Updated userController

--- controller/userController.js
+++ controller/userController.js
@@ -2,18 +2,18 @@ const userDB = require('../db/userDB');
 
 // Update user information
 exports.updateUserInfo = async (req, res) => {
-  const userId = req.params.id;
+  const userID = req.user.id;
   const userInfo = req.body;
 
   try {
-    const updatedUser = await userDB.updateUser(userId, userInfo);
+    const updatedUser = await userDB.updateUser(userID, userInfo);
     if (!updatedUser) {
       return res.status(404).json({ message: 'User not found' });
     }
     res.status(200).json(updatedUser);
   } catch (error) {
     console.error('Error updating user info:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: error.message || 'Internal server error' });
   }
 };
 
