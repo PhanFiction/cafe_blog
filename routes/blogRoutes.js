94a5cc2ec11f8619dd988f863756a531f23f5863
Steve <81824005+PhanFiction@users.noreply.github.com>
2026-01-25 23:22:48 -0800

    Updated blogRoutes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,8 +3,8 @@ const middleware = require('../middleware/index');
 const router = require('express').Router();
 
 // Routes for blog operations
-router.post('/create', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.createBlog);
-router.put('/update/:id', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.updateBlog);
+router.post('/create', middleware.checkAuth, blogController.createBlog);
+router.put('/:id', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.updateBlog);
 router.delete('/delete/:id', (middleware.checkAuth, middleware.checkBlogOwnerShip), blogController.deleteBlog);
 router.get('/:id', blogController.getBlogById);
 router.get('/', blogController.getAllBlogs);
