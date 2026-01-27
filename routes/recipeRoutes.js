8d128f107d706450d6b4fb31aea54b7a40c8f21d
Steve <81824005+PhanFiction@users.noreply.github.com>
2026-01-26 23:39:13 -0800

    Updated recipe routes

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -5,8 +5,8 @@ const router = require('express').Router();
 // Routes for recipe operations
 router.get('/', recipeController.getAllRecipes);
 router.get('/:id', recipeController.getRecipeById);
-router.post('/create', recipeController.createRecipe);
-router.put('/:id', middleware.validateRecipe, middleware.checkRecipeOwnership, recipeController.updateRecipe);
-router.delete('/:id', middleware.checkRecipeOwnership, recipeController.deleteRecipe);
+router.post('/create', middleware.checkAuth, recipeController.createRecipe);
+router.put('/:id', (middleware.validateRecipe, middleware.checkRecipeOwnership), recipeController.updateRecipe);
+router.delete('/:id', (middleware.checkAuth, middleware.checkRecipeOwnership), recipeController.deleteRecipe);
 
 module.exports = router;
